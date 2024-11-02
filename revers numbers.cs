f9906de40d069ab0fc0459623bbb1f96f8569140
Mohamad7277 <mditmdi566@gmail.com>
2024-11-02 21:24:20 +0330

    Add files via upload

---
+++ revers numbers.cs
@@ -0,0 +1,16 @@
+﻿int n = 10;
+int m = 45;
+for (int i = n; i <= m; i++)
+{
+Console.WriteLine($"{i} is {Reverse(i)}");
+}
+static int Reverse(int number)
+{
+        int reversed = 0;
+        while (number > 0)
+        {
+            reversed = reversed * 10 + number % 10;
+            number /= 10;
+        }
+    return reversed;
+}
