5d39f91a18ed25084c9fe420e25d68d1d8e5a7be
Mohamad7277 <mditmdi566@gmail.com>
2024-11-02 22:00:02 +0330

    Add files via upload

---
+++ Read number.cs
@@ -0,0 +1,15 @@
+﻿char key;
+
+do
+{
+    do
+    {
+        key = Console.ReadKey().KeyChar;
+
+    } while (!char.IsDigit(key) && key != '5');
+
+    if (key != '5')
+    {
+        Console.Write(key);
+    }
+} while (key != '5');
