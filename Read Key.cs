79654a1e04ab2530b79bde4f78a78e3fbc791828
Mohamad7277 <mditmdi566@gmail.com>
2024-11-02 19:11:53 +0330

    Add files via upload

---
+++ Read Key.cs
@@ -0,0 +1,15 @@
+﻿char key;
+
+do
+{
+    do
+    {
+        key = Console.ReadKey().KeyChar;
+
+    } while (!char.IsDigit(key) && key != 'R');
+
+    if (key !=  'R')
+    {
+        Console.Write(key);
+    }
+} while (key != 'R');
