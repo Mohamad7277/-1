866e4c577188720df9451595f1532e92d245a26f
Mohamad7277 <mditmdi566@gmail.com>
2024-11-02 20:24:29 +0330

    Add files via upload

---
+++ prime numbers.cs
@@ -0,0 +1,12 @@
+﻿int n = 2;
+int m = 200;
+for (int i = n; i <= m; i++)
+{
+if (i < 2) continue;
+bool isPrime = true;
+for (int j = 2; j * j <= i; j++)
+{
+if (i % j == 0) isPrime = false;
+}
+if (isPrime) Console.WriteLine(i);
+}
